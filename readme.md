61fddb1af2831fd1c607da0d4349140ff292920d
masao.setogawa <ms.job.hunting@gmail.com>
2024-06-02 01:17:23 +0900

    Add Oauth

---
+++ readme.md
@@ -0,0 +1,9 @@
+# 「OAuth、OAuth 認証、OpenID Connect の違いを整理して理解できる本」のチュートリアルを Express で実装
+
+### 「OAuth、OAuth 認証、OpenID Connect の違いを整理して理解できる本」
+
+https://authya.booth.pm/items/1550861
+
+### google cloud console
+
+https://console.cloud.google.com/
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require("express");
+import express from "express";
+import { clientId, clientSecret } from "./config-local.js";
 const app = express();
 
 const server = app.listen(3000, function () {
@@ -6,7 +7,61 @@ const server = app.listen(3000, function () {
 });
 
 app.get("/oauth-start", async function (req, res, next) {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await response.json();
-  res.json(data);
+  // 認可リクエストパラメータ
+  const baseUrl = "https://accounts.google.com/o/oauth2/v2/auth";
+  const responseType = "code";
+  const state = "xyz";
+  const scope = "https://www.googleapis.com/auth/photoslibrary.readonly";
+  const redirectUri = "http://127.0.0.1:3000/callback";
+  // パラメータくみ上げ
+  const authReqUrl = `${baseUrl}?response_type=${responseType}&client_id=${clientId}&state=${state}&scope=${scope}&redirect_uri=${redirectUri}`;
+  res.redirect(authReqUrl);
+});
+
+app.get("/callback", async function (req, res, next) {
+  console.log(req.url);
+  console.log(req.query.code);
+
+  // トークンリクエストパラメータ
+  const tokenRecBaseUrl = "https://www.googleapis.com/oauth2/v4/token";
+  const responseType = "code";
+  const redirectUri = "http://127.0.0.1:3000/callback";
+  const grantType = "authorization_code";
+  const code = req.query.code;
+
+  // トークンリクエスト準備
+  const bodyObject = {
+    client_id: clientId,
+    client_secret: clientSecret,
+    grant_type: grantType,
+    redirect_uri: redirectUri,
+    code: code,
+  };
+  const body = Object.keys(bodyObject)
+    .map((key) => key + "=" + encodeURIComponent(bodyObject[key]))
+    .join("&");
+  const headers = {
+    Accept: "application/json",
+    "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+  };
+  // トークンリクエスト
+  const tokenResponse = await fetch(tokenRecBaseUrl, {
+    method: "POST",
+    headers,
+    body,
+  });
+  const tokenResData = await tokenResponse.json();
+  console.log(tokenResData);
+
+  // リソースリクエスト
+  const resourceRecBaseUrl = "https://photoslibrary.googleapis.com/v1/albums";
+  const accessToken = tokenResData.access_token;
+
+  const resourceResponse = await fetch(resourceRecBaseUrl, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+  const resourceData = await resourceResponse.json();
+  console.log(resourceData);
+  res.json(resourceData);
 });
