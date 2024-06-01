4fbdaad962582cc4e725ce1cbcaf17c93a4b2c09
masao.setogawa <ms.job.hunting@gmail.com>
2024-06-01 22:30:42 +0900

    Add fetch

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 
 const server = app.listen(3000, function () {
-  console.log("listening port is " + server.address().port);
+  console.log("goto http://localhost:3000/oauth-start ");
 });
 
-app.get("/oauth-start", function (req, res, next) {
-  res.json("HELLO EXPRESS");
+app.get("/oauth-start", async function (req, res, next) {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  res.json(data);
 });
