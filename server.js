99cbac67384f05bc7dfa24592288928696107674
masao.setogawa <ms.job.hunting@gmail.com>
2024-06-02 15:36:03 +0900

    refactoring

--- server.js
+++ server.js
@@ -14,52 +14,41 @@ app.use(
 app.get("/oauth-start", async function (req, res, next) {
   // 認可リクエストパラメータ
   const baseUrl = "https://accounts.google.com/o/oauth2/v2/auth";
-  const responseType = "code";
-  const scope = "https://www.googleapis.com/auth/photoslibrary.readonly";
-  const redirectUri = "http://127.0.0.1:3000/callback";
-  const state = randomUUID();
+  const queryObject = {
+    response_type: "code",
+    client_id: clientId,
+    state: randomUUID(),
+    scope: "https://www.googleapis.com/auth/photoslibrary.readonly",
+    redirect_uri: "http://127.0.0.1:3000/callback",
+  };
 
   // sessionにstateを保存
-  req.session.state = state;
+  req.session.state = queryObject.state;
   console.log(req.session);
   console.log(req.session.id);
 
-  // トークンリクエスト準備
-  const queryObject = {
-    response_type: responseType,
-    client_id: clientId,
-    state: state,
-    scope: scope,
-    redirect_uri: redirectUri,
-  };
-  const query = new URLSearchParams(queryObject).toString();
-
   // パラメータくみ上げ
+  const query = new URLSearchParams(queryObject).toString();
   const authReqUrl = `${baseUrl}?${query}`;
   res.redirect(authReqUrl);
 });
 
 app.get("/callback", async function (req, res, next) {
+  console.log(req.url);
   console.log(req.session);
   console.log(req.session.id);
-  console.log(req.url);
-  console.log(req.query.code);
+
+  // stateの検証
   if (req.query.state !== req.session.state) throw new Error("Invalid state.");
 
   // トークンリクエストパラメータ
   const tokenRecBaseUrl = "https://www.googleapis.com/oauth2/v4/token";
-  const responseType = "code";
-  const redirectUri = "http://127.0.0.1:3000/callback";
-  const grantType = "authorization_code";
-  const code = req.query.code;
-
-  // トークンリクエスト準備
   const bodyObject = {
     client_id: clientId,
     client_secret: clientSecret,
-    grant_type: grantType,
-    redirect_uri: redirectUri,
-    code: code,
+    grant_type: "authorization_code",
+    redirect_uri: "http://127.0.0.1:3000/callback",
+    code: req.query.code,
   };
   const body = new URLSearchParams(bodyObject).toString();
   const headers = {
